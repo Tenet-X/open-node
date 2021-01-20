28b4da3424e160b4b8bae038051cc3db83de3ec4
leisureroad <liufan@google.com>
2021-01-21 02:41:30 +0800

    update

--- build.sh
+++ build.sh
@@ -42,7 +42,7 @@ read -t 30 -p "Please input GKE cluster name: " CLUSTER_NAME
 echo "GKE cluster name: $CLUSTER_NAME"
 
 export PROJECT_ID=$PROJECT_ID
-export GCP_REGION=$GCP_REGION;
-export NODE_NAME=$NODE_NAME;
-export CLUSTER_NAME=$CLUSTER_NAME;
+export GCP_REGION=$GCP_REGION
+export NODE_NAME=$NODE_NAME
+export CLUSTER_NAME=$CLUSTER_NAME
 gcloud builds submit --substitutions=_PROJECT_ID=${PROJECT_ID},_GCP_REGION=${GCP_REGION},_NODE_NAME=${NODE_NAME},_CLUSTER_NAME=${CLUSTER_NAME}
