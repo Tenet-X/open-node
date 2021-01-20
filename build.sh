e616f7fb3a4ec954e425194ed3069a84621295f2
leisureroad <liufan@google.com>
2021-01-21 02:46:16 +0800

    update build script

--- build.sh
+++ build.sh
@@ -1,8 +1,15 @@
 #!/bin/bash -xe
+read -t 30 -p "Please input Phala node name: " NODE_NAME
+echo "GCP project id: $NODE_NAME"
+
+read -t 30 -p "Please input GKE cluster name: " CLUSTER_NAME
+echo "GKE cluster name: $CLUSTER_NAME"
+
+export NODE_NAME=$NODE_NAME
+export CLUSTER_NAME=$CLUSTER_NAME
+
 PROJECT_ID=$(gcloud config get-value project)
-
 PROJECT_NUMBER=$(gcloud projects list --filter="$PROJECT_ID" --format="value(PROJECT_NUMBER)")
-
 GCP_REGION=$(gcloud config get-value compute/region)
 
 gcloud projects add-iam-policy-binding local-alignment-284902 \
@@ -29,20 +36,4 @@ gcloud projects add-iam-policy-binding local-alignment-284902 \
     --member=serviceAccount:${PROJECT_NUMBER}@cloudbuild.gserviceaccount.com \
      --role roles/compute.storageAdmin
 
-# read -t 30 -p "Please input GCP project id: " PROJECT_ID
-# echo "GCP project id: $PROJECT_ID"
-#
-# read -t 30 -p "Please input GCP region: " GCP_REGION
-# echo "GCP region: $GCP_REGION"
-
-read -t 30 -p "Please input Phala node name: " NODE_NAME
-echo "GCP project id: $NODE_NAME"
-
-read -t 30 -p "Please input GKE cluster name: " CLUSTER_NAME
-echo "GKE cluster name: $CLUSTER_NAME"
-
-export PROJECT_ID=$PROJECT_ID
-export GCP_REGION=$GCP_REGION
-export NODE_NAME=$NODE_NAME
-export CLUSTER_NAME=$CLUSTER_NAME
 gcloud builds submit --substitutions=_PROJECT_ID=${PROJECT_ID},_GCP_REGION=${GCP_REGION},_NODE_NAME=${NODE_NAME},_CLUSTER_NAME=${CLUSTER_NAME}
