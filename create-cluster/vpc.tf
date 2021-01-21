3b50687bfd0a347d3718ff25ec502fa552d8ec6e
leisureroad <liufan@google.com>
2021-01-21 10:50:28 +0800

    update

--- create-cluster/vpc.tf
+++ create-cluster/vpc.tf
@@ -24,7 +24,7 @@ resource "google_compute_network" "vpc" {
 
 # Subnet
 resource "google_compute_subnetwork" "subnet" {
-  name          = "${var.project_id}-subnet"
+  name          = "${var.cluster_name}-subnet"
   region        = var.region
   network       = google_compute_network.vpc.name
   ip_cidr_range = "10.10.0.0/24"
