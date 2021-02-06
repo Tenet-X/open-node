ff0727554aabd237cfa6c79f44698131cd4ea386
leisureroad <liufan@google.com>
2021-02-06 15:08:13 +0800

    add validator script

--- add-validator-node.sh
+++ add-validator-node.sh
@@ -5,6 +5,9 @@ echo "Full node name: $NODE_NAME"
 read -t 30 -p "Please input GKE cluster name: " CLUSTER_NAME
 echo "GKE cluster name: $CLUSTER_NAME"
 
+read -t 30 -p "Please input full node name: " FULL_NODE_NAME
+echo "Full node name: $FULL_NODE_NAME"
+
 export NODE_NAME=$NODE_NAME
 export CLUSTER_NAME=$CLUSTER_NAME
 
@@ -12,4 +15,9 @@ PROJECT_ID=$(gcloud config get-value project)
 PROJECT_NUMBER=$(gcloud projects list --filter="$PROJECT_ID" --format="value(PROJECT_NUMBER)")
 GCP_REGION=$(gcloud config get-value compute/region)
 
-gcloud builds submit --config=add-node/cloudbuild.yaml --substitutions=_PROJECT_ID=${PROJECT_ID},_GCP_REGION=${GCP_REGION},_NODE_NAME=${NODE_NAME},_CLUSTER_NAME=${CLUSTER_NAME}
+gcloud builds submit --config=add-node/cloudbuild.yaml \
+--substitutions=_PROJECT_ID=${PROJECT_ID}, \
+_GCP_REGION=${GCP_REGION}, \
+_NODE_NAME=${NODE_NAME}, \
+_CLUSTER_NAME=${CLUSTER_NAME}, \
+_FULL_NODE_NAME=${FULL_NODE_NAME}
