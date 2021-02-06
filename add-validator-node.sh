486dfeb1b04a62327f68755acfe40b150d78bc75
leisureroad <liufan@google.com>
2021-02-06 20:46:45 +0800

    Beautify code

--- add-validator-node.sh
+++ add-validator-node.sh
@@ -1,15 +1,15 @@
 #!/bin/bash -xe
-read -t 30 -p "Please input validator node name: " NODE_NAME
-echo "Validator node name: $NODE_NAME"
-
 read -t 30 -p "Please input GKE cluster name: " CLUSTER_NAME
 echo "GKE cluster name: $CLUSTER_NAME"
+
+read -t 30 -p "Please input validator node name: " NODE_NAME
+echo "Validator node name: $NODE_NAME"
 
 read -t 30 -p "Please input full node name: " FULL_NODE_NAME
 echo "Full node name: $FULL_NODE_NAME"
 
-export NODE_NAME=$NODE_NAME
 export CLUSTER_NAME=$CLUSTER_NAME
+export NODE_NAME=$NODE_NAME
 
 PROJECT_ID=$(gcloud config get-value project)
 PROJECT_NUMBER=$(gcloud projects list --filter="$PROJECT_ID" --format="value(PROJECT_NUMBER)")
--- setup.sh
+++ setup.sh
@@ -1,12 +1,12 @@
 #!/bin/bash -xe
-read -t 30 -p "Please input Phala node name: " NODE_NAME
-echo "Full node name: $NODE_NAME"
-
 read -t 30 -p "Please input GKE cluster name: " CLUSTER_NAME
 echo "GKE cluster name: $CLUSTER_NAME"
 
-export NODE_NAME=$NODE_NAME
+read -t 30 -p "Please input full node name: " NODE_NAME
+echo "Full node name: $NODE_NAME"
+
 export CLUSTER_NAME=$CLUSTER_NAME
+export NODE_NAME=$NODE_NAME
 
 PROJECT_ID=$(gcloud config get-value project)
 PROJECT_NUMBER=$(gcloud projects list --filter="$PROJECT_ID" --format="value(PROJECT_NUMBER)")
