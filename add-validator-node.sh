3958f501c3cbbde5dd1600fa93d020700530169d
leisureroad <liufan@google.com>
2021-02-07 16:31:20 +0800

    add session key & mon port

--- add-validator-node.sh
+++ add-validator-node.sh
@@ -8,6 +8,9 @@ echo "Validator node name: $NODE_NAME"
 read -t 30 -p "Please input full node name: " FULL_NODE_NAME
 echo "Full node name: $FULL_NODE_NAME"
 
+read -t 30 -p "Please input your session key: " SESSION_KEY
+echo "Session key is: $SESSION_KEY"
+
 export CLUSTER_NAME=$CLUSTER_NAME
 export NODE_NAME=$NODE_NAME
 
@@ -16,4 +19,4 @@ PROJECT_NUMBER=$(gcloud projects list --filter="$PROJECT_ID" --format="value(PRO
 GCP_REGION=$(gcloud config get-value compute/region)
 
 gcloud builds submit --config=add-validator-node/cloudbuild.yaml \
---substitutions=_PROJECT_ID=${PROJECT_ID},_GCP_REGION=${GCP_REGION},_NODE_NAME=${NODE_NAME},_CLUSTER_NAME=${CLUSTER_NAME},_FULL_NODE_NAME=${FULL_NODE_NAME}
+--substitutions=_PROJECT_ID=${PROJECT_ID},_GCP_REGION=${GCP_REGION},_NODE_NAME=${NODE_NAME},_CLUSTER_NAME=${CLUSTER_NAME},_FULL_NODE_NAME=${FULL_NODE_NAME},_SESSION_KEY=${SESSION_KEY}
